47e9467b3bd00b9ebdcb1c227f19648d59199f35
Ubuntu <ubuntu@ip-172-31-20-173.us-west-2.compute.internal>
2016-03-09 17:14:37 +0000

    added database code

--- assignments/databaseUI/server.js
+++ assignments/databaseUI/server.js
@@ -1,19 +1,45 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var pool = mysql.createPool({
+   host: 'localhost',
+   user: 'student',
+   password: 'default',
+   database: 'student'
+});
+
+pool.query("DROP TABLE IF EXISTS todo", function(err) {
+   var createString = "CREATE TABLE todo(" + 
+   "id INT PRIMARY KEY AUTO_INCREMENT," + 
+   "name VARCHAR(255) NOT NULL," +
+   "done BOOLEAN," + 
+   "due DATE)";
+   pool.query(createString, function(err) {
+      if (err) console.log(err);
+      console.log("todo table created");
+   });
+});
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 4000);
+app.set('port', 2000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	res.render('form');
+});
+
+app.get('/tasks', function(req, res) {
+
+});
+
+app.post('/tasks', function(req, res) {
+
 });
 
 app.post('/', function(req, res){
@@ -32,5 +58,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-	console.log('Express started on port 4000');
+	console.log('Express started on port 2000');
 });
