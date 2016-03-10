3caf066dfe44003b4ed677599321beac00b33070
Ubuntu <ubuntu@ip-172-31-21-101.us-west-2.compute.internal>
2016-03-10 13:20:56 +0000

    Updates to the database server

--- assignments/databaseUI/public/js/app.js
+++ assignments/databaseUI/public/js/app.js
@@ -1,1 +1,27 @@
-console.log("loaded!");
+$(document).ready(function(){
+   $.ajax({
+      url: '/tasks',
+      dataType: 'json',
+      success: function(data, textStatus, jqXHR) {
+         console.log(data);
+      }
+   });   
+
+   $('.add-task').on('click', function(e) {
+      e.preventDefault();
+      var newTask = {};
+      newTask['name'] = $('.name').val();
+      newTask['rep'] = $('.rep').val();
+      newTask['weight'] = $('.weight').val();
+      newTask['date'] = $('.date').val();
+      newTask['units'] = $('.units').val();
+      $.ajax({
+         url: '/tasks',
+         method: 'POST',
+         data: newTask,
+         success: function(data, textStatus, jqXHR) {
+            console.log(data);
+         }
+      });   
+   });       
+});
--- assignments/databaseUI/server.js
+++ assignments/databaseUI/server.js
@@ -12,8 +12,10 @@ pool.query("DROP TABLE IF EXISTS todo", function(err) {
    var createString = "CREATE TABLE todo(" + 
    "id INT PRIMARY KEY AUTO_INCREMENT," + 
    "name VARCHAR(255) NOT NULL," +
-   "done BOOLEAN," + 
-   "due DATE)";
+   "rep INT," +
+   "weight INT," +
+   "units BOOLEAN," + 
+   "date DATE)";
    pool.query(createString, function(err) {
       if (err) console.log(err);
       console.log("todo table created");
@@ -25,25 +27,59 @@ var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 2000);
+app.set('port', 4000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-	res.render('form');
+   res.render('form');
 });
 
 app.get('/tasks', function(req, res) {
-
+   var ctx = {};
+   pool.query('SELECT * FROM todo', function(err, rows, fields) {
+      if (err) {
+         next(err);
+         return;
+      }
+      ctx.results = JSON.stringify(rows);
+      res.send(ctx);
+   });
 });
 
 app.post('/tasks', function(req, res) {
-
+   var body = req.body;
+   var name = body.name;
+   var reps = body.rep;
+   var weight = body.weight;
+   var date = body.date;
+   var units = body.units === 'kg' ? 0 : 1;
+   var values = "'" + name + "'," + reps + ',' + weight + ',' + date + ',' + units;
+   pool.query('INSERT INTO todo(name, rep, weight, date, units) VALUES (' + values + ');', function(err, rows, fields) {
+      if (err) {
+         console.log(err);
+         return;
+      }
+      res.send('post successful');
+   });
 });
 
 app.post('/', function(req, res){
-	res.render('form');
+   res.render('form');
+});
+
+app.delete('/tasks', function(req, res) {
+   var id = req.id;
+   var ctx = {};
+   pool.query('DELETE FROM todo WHERE id = ' + id, function(err, rows, fields) {
+      if (err) {
+         next(err);
+         return;
+      }
+      ctx.results = JSON.stringify(rows);
+      res.send(ctx);
+   });
 });
 
 app.use(function(req, res){
@@ -58,5 +94,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-	console.log('Express started on port 2000');
+	console.log('Express started on port 4000');
 });
